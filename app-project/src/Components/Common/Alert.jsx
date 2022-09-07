881e2e08e566eabc534bcd447d01ab9b71a930fc
SourabhMPatrikar <sourabhmpatrikar@gmail.com>
2022-09-07 14:29:41 +0530

    Common alert component change.

--- app-project/src/Components/Common/Alert.jsx
+++ app-project/src/Components/Common/Alert.jsx
@@ -2,7 +2,19 @@ import React from "react";
 const Alert = ({ alertType, alertText, alertTextType }) => {
 
   let alertVariant =
-    alertType === "success"
+      alertType === "default"
+      ? "alert-default"
+      : alertType === "info"
+      ? "alert-info"
+      : alertType === "primary"
+      ? "alert-primary"
+      : alertType === "secondary"
+      ? "alert-secondary"
+      : alertType === "dark"
+      ? "alert-dark"
+      : alertType === "light"
+      ? "alert-light"
+      : alertType === "success"
       ? "alert-success"
       : alertType === "danger"
       ? "alert-danger"
@@ -22,6 +34,7 @@ const Alert = ({ alertType, alertText, alertTextType }) => {
   return (
     <div className={`alert ${alertVariant}`}>
       <p className={alertTxtType}>{alertText}</p>
+      <span className="close"></span>
     </div>
   );
 };
