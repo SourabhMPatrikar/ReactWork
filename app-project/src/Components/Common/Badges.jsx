97d4bfcfe5c1c7970ad72ceba52dbfe36bd5bd11
SourabhMPatrikar <sourabhmpatrikar@gmail.com>
2022-09-07 14:32:22 +0530

    Common badges component change.

---
+++ app-project/src/Components/Common/Badges.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+const Badges = ({ variant, badgesText, badgeColor }) => {
+  switch (variant) {
+    case "default":
+      variant = "badge-default";
+      break;
+    case "info":
+      variant = "badge-info";
+      break;
+    case "primary":
+      variant = "badge-primary";
+      break;
+    case "secondary":
+      variant = "badge-secondary";
+      break;
+    case "light":
+      variant = "badge-light";
+      break;
+    case "dark":
+      variant = "badge-dark";
+      break;
+    case "success":
+      variant = "badge-success";
+      break;
+    case "danger":
+      variant = "badge-danger";
+      break;
+    case "warn":
+      variant = "badge-warn";
+      break;
+    default:
+      variant = "";
+  }
+  // let badgesClass = badgesVariant === "" ? "" :
+  // badgesVariant === "default" ? "badge-default" :
+  // badgesVariant === "info" ? "badge-info" :
+  // badgesVariant === "primary" ? "badge-primary" :
+  // badgesVariant === "secondary" ? "badge-secondary" :
+
+  // badgesVariant === "light" ? "badge-light" :
+  // badgesVariant === "dark" ? "badge-dark" :
+
+  // badgesVariant === "success" ? "badge-success" :
+  // badgesVariant === "danger" ? "badge-danger" :
+  // badgesVariant === "warn" ? "badge-warn" : "";
+
+  let badgeColorClass =
+    badgeColor === "light" ? "white" : badgeColor === "dark" ? "black" : "";
+  return (
+    <span className={`badge ${variant} ${badgeColorClass}`}>{badgesText}</span>
+  );
+};
+export default Badges;
--- app-project/src/Components/Common/Common.jsx
+++ app-project/src/Components/Common/Common.jsx
@@ -1,37 +1,99 @@
 import React from "react";
 import Button from "./Button";
 import Alert from "./Alert";
+import Badges from "./Badges";
 const Common = () => {
   return (
     <div className="common-component">
       <div className="row">
         <div className="title">
+          <h4>Badges</h4>
+        </div>
+        <div className="combine">
+          <div className="badge-collection">
+            <Badges variant="" badgesText="normal" />
+            <Badges variant="default" badgesText="default" />
+            <Badges variant="info" badgesText="info" />
+            <Badges variant="primary" badgesText="primary" />
+            <Badges variant="secondary" badgesText="secondary" />
+
+            <Badges variant="dark" badgesText="dark" />
+            <Badges variant="light" badgesText="light" />
+
+            <Badges variant="success" badgesText="success" />
+            <Badges variant="danger" badgesText="danger" />
+            <Badges variant="warn" badgesText="warn" />
+          </div>
+          
+          <div className="badge-collection">
+            <Badges variant="" badgeColor="light" badgesText="normal"  />
+            <Badges variant="default" badgeColor="light" badgesText="default" />
+            <Badges variant="info" badgeColor="light" badgesText="info" />
+            <Badges variant="primary" badgeColor="light" badgesText="primary" />
+            <Badges variant="secondary" badgeColor="light" badgesText="secondary" />
+
+            <Badges variant="dark" badgeColor="light" badgesText="dark" />
+            <Badges variant="light" badgeColor="light" badgesText="light" />
+
+            <Badges variant="success" badgeColor="light" badgesText="success" />
+            <Badges variant="danger" badgeColor="light" badgesText="danger" />
+            <Badges variant="warn" badgeColor="light" badgesText="warn" />
+          </div>
+          
+          <div className="badge-collection">
+            <Badges variant="" badgeColor="dark" badgesText="normal"  />
+            <Badges variant="default" badgeColor="dark" badgesText="default" />
+            <Badges variant="info" badgeColor="dark" badgesText="info" />
+            <Badges variant="primary" badgeColor="dark" badgesText="primary" />
+            <Badges variant="secondary" badgeColor="dark" badgesText="secondary" />
+
+            <Badges variant="dark" badgeColor="dark" badgesText="dark" />
+            <Badges variant="light" badgeColor="dark" badgesText="light" />
+
+            <Badges variant="success" badgeColor="dark" badgesText="success" />
+            <Badges variant="danger" badgeColor="dark" badgesText="danger" />
+            <Badges variant="warn" badgeColor="dark" badgesText="warn" />
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="title">
           <h4>Alert</h4>
         </div>
-        <div className="btn-box">
-          <Alert alertType="" alertText="Normal Message." alertTextType="" />
-          <Alert alertType="success" alertText="Success Message." alertTextType="bold" />
-          <Alert alertType="danger" alertText="Danger Message." alertTextType="italic" />
-          <Alert alertType="warn" alertText="Warn Message." alertTextType="under-line" />
+        <div className="combine">
+          <Alert alertType="normal" alertText="Message normal." alertTextType="" />
+          <Alert alertType="default" alertText="Message default." alertTextType="" />
+          <Alert alertType="info" alertText="Message info." alertTextType="italic" />
+          <Alert alertType="primary" alertText="Message primary." alertTextType="" />
+          <Alert alertType="secondary" alertText="Message secondary." alertTextType="" />
+
+          <Alert alertType="dark" alertText="Message dark." alertTextType="" />
+          <Alert alertType="light" alertText="Message light." alertTextType="" />
+
+          <Alert alertType="success" alertText="Message success." alertTextType="bold" />
+          <Alert alertType="danger" alertText="Message danger." alertTextType="under-line" />
+          <Alert alertType="warn" alertText="Message warn." alertTextType="" />
         </div>
       </div>
       <div className="row">
         <div className="title">
           <h4>Button</h4>
         </div>
-        <div className="btn-box">
-          <Button btnText="First" />
-          <Button btnText="Second" variant="default" />
-          <Button btnText="Second" variant="info" />
-          <Button btnText="Second" variant="primary" />
-          <Button btnText="Second" variant="secondary" />
+        <div className="combine">
+          <div className="btn-box">
+            <Button btnText="normal" variant="normal" />
+            <Button btnText="default" variant="default" />
+            <Button btnText="info" variant="info" />
+            <Button btnText="primary" variant="primary" />
+            <Button btnText="secondary" variant="secondary" />
 
-          <Button btnText="dark" variant="dark" />
-          <Button btnText="light" variant="light" />
+            <Button btnText="dark" variant="dark" />
+            <Button btnText="light" variant="light" />
 
-          <Button btnText="success" variant="success" />
-          <Button btnText="danger" variant="danger" />
-          <Button btnText="warn" variant="warn" />
+            <Button btnText="success" variant="success" />
+            <Button btnText="danger" variant="danger" />
+            <Button btnText="warn" variant="warn" />
+          </div>
         </div>
       </div>
     </div>
