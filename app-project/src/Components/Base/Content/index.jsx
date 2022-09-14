0382d59424b56d3f48f62235e764e68e66fbcf49
SourabhMPatrikar <sourabhmpatrikar@gmail.com>
2022-09-14 19:09:05 +0530

    Actwo : Base CSS Changes.

--- app-project/src/Components/Base/Content/index.jsx
+++ app-project/src/Components/Base/Content/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Common from "../../Common/ComponentTheme/";
+import Common from "../../Common/Common";
 const Content = () => {
     return(
         <div id="content">
